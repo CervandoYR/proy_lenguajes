d1864bfc07b536f43835c7e7f1b0f286f7e24d48
CervandoYR <yactayocervando@gmail.com>
2026-07-16 14:30:58 -0500

    Cambios

--- backend/src/test/java/pe/edu/utp/ecommerce/EcommerceApplicationTests.java
+++ backend/src/test/java/pe/edu/utp/ecommerce/EcommerceApplicationTests.java
@@ -6,8 +6,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcommerceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @org.springframework.beans.factory.annotation.Autowired
+    private pe.edu.utp.ecommerce.repository.UsuarioRepository usuarioRepository;
 
+    @org.springframework.beans.factory.annotation.Autowired
+    private javax.sql.DataSource dataSource;
+
+	@Test
+	void contextLoads() throws Exception {
+        System.out.println("=== DIAGNOSTICO BASE DE DATOS LOCAL ===");
+        try (java.sql.Connection conn = dataSource.getConnection()) {
+            System.out.println("JDBC URL CONECTADO: " + conn.getMetaData().getURL());
+            System.out.println("USER CONECTADO: " + conn.getMetaData().getUserName());
+        }
+        System.out.println("LISTA DE USUARIOS EN ESTA BASE DE DATOS:");
+        usuarioRepository.findAll().forEach(u -> 
+            System.out.println(" -> ID: " + u.getId() + " | Email: " + u.getEmail() + " | Nombre: " + u.getNombre())
+        );
+        System.out.println("=== FIN DIAGNOSTICO ===");
+	}
 }
