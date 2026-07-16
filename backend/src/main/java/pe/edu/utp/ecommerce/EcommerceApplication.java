c1e4e14c9bc41ecd5dae7368ee35d4ccecfe0974
CervandoYR <yactayocervando@gmail.com>
2026-07-16 00:56:40 -0500

    Solucionado error de hora en railway

--- backend/src/main/java/pe/edu/utp/ecommerce/EcommerceApplication.java
+++ backend/src/main/java/pe/edu/utp/ecommerce/EcommerceApplication.java
@@ -1,12 +1,19 @@
 package pe.edu.utp.ecommerce;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableAsync
 public class EcommerceApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
